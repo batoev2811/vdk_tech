8a0c6a863046e37fb64ecc75654054b2bebab59e
Bato Batoev <b.batoev@mail.ru>
2025-08-14 15:00:24 +0800

    Удалил файлы авторизации, т.к будет использоваться авторизация биллинга

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vdk_tech/ui/pages/authentication/authentication.dart';
 import 'package:vdk_tech/ui/pages/home.dart';
 
 void main() => runApp(
