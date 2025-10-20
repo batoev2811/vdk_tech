fc275f5de0723b1dab5884d1bcbd7284d4910bc4
Bato Batoev <b.batoev@mail.ru>
2025-10-20 16:13:12 +0800

    Меняем загрузку файлов

--- lib/features/presentation/page/steps/step_one.dart
+++ lib/features/presentation/page/steps/step_one.dart
@@ -36,47 +36,49 @@ class _StepOneState extends State<StepOneState> {
           ),
           SizedBox(height: 10),
           FormatFile(),
-          Text(
-            'Копия паспорта (доверенность, подтверждающая полномочия лица, подписавшего запрос)', 
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 10),
-          DropzoneWidget(),
-          SizedBox(height: 10),
-          Text(
-            'План земельного участка в масштабе 1:500 и 1:2000 со всеми наземными и подземными коммуникациями' 
-            '\n(запрашивается в Комитете по архитектуре и градостроительству, Администрации г. Улан-Удэ ул. Бабушкина д.25)', 
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 10),
-          DropzoneWidget(),
-          SizedBox(height: 10),
-          Text(
-            'Расчет нагрузок водопотребления и (или) водоотведения м3/час, л/сек, л/сут' 
-            '\n(запрашивается в любой проектной организации)', 
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 10),
-          DropzoneWidget(),
-          SizedBox(height: 10),
-          Text(
-            'ИНН/ОГРН', 
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 10),
-          DropzoneWidget(),
-          SizedBox(height: 10),
-          Text(
-            'Копии правоустанавливающих и правоудостоверяющих документов на подключаемы земельный участок и объект, ранее построенный и введенный в эксплуатацию. ' 
-            '\n(При предоставлении в качестве, правоудостоверяющего документа выписки из ЕГРН, то должны быть получены не ранее чем за 30 кален.дней до дня направления запроса)', 
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 10),
-          DropzoneWidget(),
-          SizedBox(height: 10),
-          Text(
-            'Иные документы', 
-            style: TextStyle(fontSize: 18),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.only(left: 10.0),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade400,
+              border: BoxBorder.all(
+                style: BorderStyle.solid,
+              )
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 5.0,
+              children: [
+                Text(
+              '- Копия паспорта (доверенность, подтверждающая полномочия лица, подписавшего запрос)', 
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              '- План земельного участка в масштабе 1:500 и 1:2000 со всеми наземными и подземными коммуникациями' 
+              '\n(запрашивается в Комитете по архитектуре и градостроительству, Администрации г. Улан-Удэ ул. Бабушкина д.25)', 
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              '- Расчет нагрузок водопотребления и (или) водоотведения м3/час, л/сек, л/сут' 
+              '\n(запрашивается в любой проектной организации)', 
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              '- ИНН/ОГРН', 
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              '- Копии правоустанавливающих и правоудостоверяющих документов на подключаемы земельный участок и объект, ранее построенный и введенный в эксплуатацию. ' 
+              '\n(При предоставлении в качестве, правоудостоверяющего документа выписки из ЕГРН, то должны быть получены не ранее чем за 30 кален.дней до дня направления запроса)', 
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              '- Иные документы (при необходимости)', 
+              style: TextStyle(fontSize: 16),
+            ),
+              ],
+            ),
           ),
           SizedBox(height: 10),
           DropzoneWidget(),
